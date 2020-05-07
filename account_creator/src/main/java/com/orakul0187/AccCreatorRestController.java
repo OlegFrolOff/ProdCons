8762d9e976a3e4df152ea59d22183bfc0be1005e
OlegFrolOff <orakul0187@gmail.com>
2020-05-07 15:41:06 +0400

    Fix dependencies

--- account_creator/src/main/java/com/orakul0187/AccCreatorRestController.java
+++ account_creator/src/main/java/com/orakul0187/AccCreatorRestController.java
@@ -3,15 +3,16 @@ package com.orakul0187;
 import com.orakul0187.entities.BankAccount;
 import com.orakul0187.factories.BankAccountFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class AccCreatorRestController {
     @Autowired
     BankAccountFactory bankAccFactory;
 
-    @GetMapping("getRandomAccount")
+    @RequestMapping("/getRandomAccount")
     public BankAccount getBankAccount() {
         return bankAccFactory.getBankAccount();
     }
--- user-cassandra-request/src/main/java/com/orakul0187/usercassandrarequest/rest/UserCassandraRestController.java
+++ user-cassandra-request/src/main/java/com/orakul0187/usercassandrarequest/rest/UserCassandraRestController.java
@@ -1,20 +1,20 @@
 package com.orakul0187.usercassandrarequest.rest;
 
-import com.orakul0187.entities.BankAccount;
 import com.orakul0187.usercassandrarequest.cassandra.DataGetter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
+@RequestMapping(value = "/")
 public class UserCassandraRestController {
     @Autowired
     DataGetter dataGetter;
 
-    @GetMapping("getAccount")
+    @RequestMapping("/getAccount")
     public String getAccount(@RequestParam String uuid){
         String response = dataGetter.getAccountData(UUID.fromString(uuid));
         return response == null ? "Account not found or more than one account found" : response;
